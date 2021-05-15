8322119d47ea2c2963af8633e2cfd7947f83bdd1
danlin <iita71737@gmail.com>
2021-05-15 18:11:28 +0800

    test

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLauout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+    res.render('view')
+})
+
+app.listen(port, () => {
+    console.log(`express is listening on localhost:${port}`)
+})
+
+// setting static files
+app.use(express.static('public'))
