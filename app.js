4fa0ca73f8a5eb5ce2c899ba9e1422b6f8717b48
danlin <iita71737@gmail.com>
2021-05-15 20:22:21 +0800

    123

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const port = 3000
 // require handlebars in the project
 const exphbs = require('express-handlebars')
 
+//require json file in the project
+const restaurantList = require('./restaurant.json')
+
 app.engine('handlebars', exphbs({ defaultLauout: 'main'}))
 app.set('view engine', 'handlebars')
 
@@ -15,8 +18,22 @@ app.use(express.static('public'))
 
 //routes setting
 app.get('/', (req, res) => {
-    res.render('view')
+    res.render('view', { restlist : restaurantList.results})
 })
+
+app.get('/restaurants/:id', (req, res) => {
+    const restaurant = restaurantList.results.find(rest => rest.id.toString() === req.params.id)
+    res.render('show', { rest :restaurant})
+})
+
+app.get('/search', (req, res) => {
+    const keyword = req.query.keyword
+    const rests = restaurantList.results.filter(rest => {
+        return rest.name.toLowerCase().includes(keyword.toLowerCase())
+    })
+    res.render('view', { restlist: rests,  keyword: keyword})
+})
+
 
 // start and listen on the Express server
 app.listen(port, () => {
