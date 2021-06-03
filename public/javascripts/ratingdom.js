fdf9d9b9bbd4ca17d1d72e33ea814960c50643d1
danlin <iita71737@gmail.com>
2021-06-04 01:13:12 +0800

    optimization for form

--- public/javascripts/ratingdom.js
+++ public/javascripts/ratingdom.js
@@ -4,3 +4,13 @@ editRating.addEventListener('input', function(event) {
   event.target.parentElement.children[1].value = event.target.value
   event.target.parentElement.children[2].value = event.target.value
 })
+
+const editRating2= document.querySelector('.edit-rating2')
+
+if (editRating2){
+  editRating2.addEventListener('input', function(event) {
+    event.target.parentElement.children[1].value = event.target.value
+    event.target.parentElement.children[2].value = event.target.value
+  })
+}
+
