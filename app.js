4e102fb7cebaab106dd49b622df51ed670827efe
iita71737 <iita71737@gmail.com>
2021-09-15 20:55:23 +0800

    feat: add bcrypt.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override')
 const routes = require('./routes')
 const app = express()
 const port = 3000
+const flash = require('connect-flash')
 
 
 const exphbs = require('express-handlebars')
@@ -33,10 +34,13 @@ app.use(methodOverride('_method'))
 
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
     // 你可以在這裡 console.log(req.user) 等資訊來觀察
     res.locals.isAuthenticated = req.isAuthenticated()
     res.locals.user = req.user
+    res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+    res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
     next()
 })
 app.use(routes)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 router.get('/login', (req, res) => {
     res.render('login')
@@ -19,6 +20,22 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
     // 取得註冊表單參數
     const { name, email, password, confirmPassword } = req.body
+    const errors = []
+    if (!name || !email || !password || !confirmPassword) {
+        errors.push({ message: '所有欄位都是必填。' })
+    }
+    if (password !== confirmPassword) {
+        errors.push({ message: '密碼與確認密碼不相符！' })
+    }
+    if (errors.length) {
+        return res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            confirmPassword
+        })
+    }
     // 檢查使用者是否已經註冊
     User.findOne({ email }).then(user => {
         // 如果已經註冊：退回原本畫面
@@ -32,11 +49,14 @@ router.post('/register', (req, res) => {
             })
         } else {
             // 如果還沒註冊：寫入資料庫
-            return User.create({
-                name,
-                email,
-                password
-            })
+            return bcrypt
+                .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+                .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+                .then(hash => User.create({
+                    name,
+                    email,
+                    password: hash // 用雜湊值取代原本的使用者密碼
+                }))
                 .then(() => res.redirect('/'))
                 .catch(err => console.log(err))
         }
@@ -45,6 +65,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
     req.logout()
+    req.flash('success_msg', '你已經成功登出。')
     res.redirect('/users/login')
 })
 
